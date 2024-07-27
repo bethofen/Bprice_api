ec50870ada674206a18db1f50c964e6c3c0b5153
beethofen <104659478+bethofen@users.noreply.github.com>
2024-07-28 01:20:59 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
    version='1.0',
    description='A useful module',
    author='Man Food',
-   author_email='---',
+   # author_email='---',
    # packages=setuptools.find_packages(),
    packages=['Bprice'],  # same as name
    install_requires=['numpy','pandas'],
