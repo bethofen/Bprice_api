db22d889d537760337d2dd0d4e8cb4343944323f
beethofen <104659478+bethofen@users.noreply.github.com>
2024-12-07 01:50:36 +0700

    Update __init__.py

--- Bprice/__init__.py
+++ Bprice/__init__.py
@@ -9,5 +9,6 @@ import pandas
 from Bprice.indicator import *
 from Bprice.edit_df import *
 from Bprice.LorentzianClassification.Classifier import *
+from Bprice.LorentzianClassification import *
 # Package metadata
 __version__ = '1.5'
