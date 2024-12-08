c82a2d95929cfa4110eb91ff2de9aef765aafcd4
beethofen <104659478+bethofen@users.noreply.github.com>
2024-12-08 18:57:54 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
    author='beethofen',
    # packages=find_packages(),
    # author_email='---',
-   packages=setuptools.find_packages(),
+   # packages=setuptools.find_packages(),
    packages=['Bprice'],  # same as name
    install_requires=['numpy','pandas'],
 )
